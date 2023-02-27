cbac731a6166fccd8e4c6c31b8934f909c20aeca
Giuseppe Castiglione <g.m.a.castiglione@gmail.com>
2023-02-26 20:00:00 -0500

    Initial customization

--- src/sections/About.js
+++ src/sections/About.js
@@ -8,23 +8,73 @@ import {Col, Container, Row } from "react-bootstrap";
 import { StaticImage } from 'gatsby-plugin-image';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
 
+import { CheckCircle } from "react-bootstrap-icons";
 
 
 const About = props => (
-  <section id="about" class="about">
+  // class="d-flex align-items-center"
+  <section id="about" class="about d-flex align-items-center">
     {/* <div class="container" data-aos="fade-up"> */}
     <Container>
+    <Row className="justify-content-center">
+      <Col lg={6}>
+        <AnimationOnScroll animateIn="animate__zoomIn" animateOnce="true">
+          <h1> <span>Minimize</span> your barriers </h1>
+        </AnimationOnScroll>
+      </Col>
+    </Row>
     <Row>
     {/* <div class="row"> */}
-      <Col lg={6} className="order-1 order-lg-2">
+      <Col lg={4} className="justify-content-center">
       {/* <div class="col-lg-6 order-1 order-lg-2" data-aos="zoom-in" data-aos-delay="100"> */}
       {/* <div class="about-img"> 
         <img src="../assets/img/about.jpg" alt=""/>
       </div> */}
-        <AnimationOnScroll animateIn="animate__zoomIn" animateOnce="true">
+        <AnimationOnScroll animateIn="animate__zoomIn" animateOnce="true" delay="400">
 
         <StaticImage
-          src="../assets/img/about.jpg"
+          // src="../assets/img/about.jpg"
+          src="../assets/img/About/gol.gif"
+          className="about-img"
+          alt=""
+        /> 
+        </AnimationOnScroll>
+
+        {/* Note: I also changed the style.css.  I removed the relative position attribute
+        in order to prevent weird offsets.   */}
+       
+      </Col>
+
+      <Col lg={4} className="justify-content-center">
+      {/* <div class="col-lg-6 order-1 order-lg-2" data-aos="zoom-in" data-aos-delay="100"> */}
+      {/* <div class="about-img"> 
+        <img src="../assets/img/about.jpg" alt=""/>
+      </div> */}
+        <AnimationOnScroll animateIn="animate__zoomIn" animateOnce="true" delay="800">
+
+        <StaticImage
+          // src="../assets/img/about.jpg"
+          src="../assets/img/About/gol.gif"
+          className="about-img"
+          alt=""
+        /> 
+        </AnimationOnScroll>
+
+        {/* Note: I also changed the style.css.  I removed the relative position attribute
+        in order to prevent weird offsets.   */}
+       
+      </Col>
+
+      <Col lg={4} className="justify-content-center">
+      {/* <div class="col-lg-6 order-1 order-lg-2" data-aos="zoom-in" data-aos-delay="100"> */}
+      {/* <div class="about-img"> 
+        <img src="../assets/img/about.jpg" alt=""/>
+      </div> */}
+        <AnimationOnScroll animateIn="animate__zoomIn" animateOnce="true" delay="1200">
+
+        <StaticImage
+          // src="../assets/img/about.jpg"
+          src="../assets/img/About/gol.gif"
           className="about-img"
           alt=""
         /> 
@@ -37,25 +87,28 @@ const About = props => (
 
       {/* </div> */}
       {/* </div> */}
-      <Col lg={6} className="pt-4 pt-lg-0 order-2 order-lg-1 content">
+      {/* <Col lg={6} className="pt-4 pt-lg-0 order-2 order-lg-1 content"> */}
       {/* <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content"> */}
-      <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
-      <p class="fst-italic">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua.
-      </p>
-      <ul>
-        <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-        <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-        <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-      </ul>
-      <p>
+
+      {/* <h3>We reduce your workflow to just three steps: </h3> */}
+      {/* <h1>Design.  Simulate.  Optimize. </h1> */}
+      {/* <br></br>
+      <p> We reduce your engineering workflow to just four steps: </p>
+      <br></br> */}
+      {/* <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li> */}
+      {/* <ul>  
+        <li><CheckCircle color="#cda45e" /> Design. </li>
+        <li><CheckCircle color="#cda45e" /> Simulate. </li>
+        <li><CheckCircle color="#cda45e" /> Optimize. </li>
+        <li><CheckCircle color="#cda45e" /> Prototype. </li>
+      </ul> */}
+      {/* <p>
         Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
         velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
         culpa qui officia deserunt mollit anim id est laborum
-      </p>
+      </p> */}
       {/* </div> */}
-      </Col>
+      {/* </Col> */}
     {/* </div> */}
     </Row>
     {/* </div> */}
--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -8,6 +8,7 @@ import {Col, Container, Row } from "react-bootstrap";
 import 'glightbox'
 
 import { AnimationOnScroll } from 'react-animation-on-scroll';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const Hero = props => (
   <section id="hero" class="d-flex align-items-center">
@@ -18,17 +19,33 @@ const Hero = props => (
     {/* <div class="col-lg-8"> */}
     {/* <Col className="col-lg-8"> */}
     
-    <Col lg={8}>
-    {/* <Animation type="fadeUp" delay={800}> */}
+    {/* <Col lg={4}>
+      
     <AnimationOnScroll animateIn="animate__zoomIn" delay="100" animateOnce="true">
-      <h1>Welcome to <span>Restaurantly</span></h1>
-      <h2>Delivering great food for more than 18 years!</h2>
+      <StaticImage
+        src="../assets/img/Hero/wohin-icon-1200x1200.png"
+        className="about-img"
+        alt=""
+      /> 
     </AnimationOnScroll>
+    </Col> */}
 
-    <AnimationOnScroll animateIn="animate__fadeInUp" delay="100" animateOnce="true">
+    <Col lg={8} className="align-self-center">
+    {/* <Animation type="fadeUp" delay={800}> */}
+    <AnimationOnScroll animateIn="animate__fadeInLeft" delay="200" animateOnce="true">
+      <h1>Design.</h1>
+    </AnimationOnScroll>
+    <AnimationOnScroll animateIn="animate__fadeInLeft" delay="400" animateOnce="true">
+      <h1>Simulate.</h1>
+    </AnimationOnScroll>
+    <AnimationOnScroll animateIn="animate__fadeInLeft" delay="600" animateOnce="true">
+      <h1><span>Optimize.</span></h1>
+    </AnimationOnScroll>
+    <AnimationOnScroll animateIn="animate__fadeInUp" delay="800" animateOnce="true">
+      <h2>We're on a mission to accelerate science</h2>
       <div class="btns">
-        <a href="#menu" class="btn-menu animated fadeInUp scrollto">Our Menu</a>
-        <a href="#book-a-table" class="btn-book animated fadeInUp scrollto">Book a Table</a>
+        <a href="#about" class="btn-menu animated fadeInUp scrollto">Learn more</a>
+        {/* <a href="#book-a-table" class="btn-book animated fadeInUp scrollto">Book a Table</a> */}
       </div>
     </AnimationOnScroll>
       
@@ -38,11 +55,11 @@ const Hero = props => (
     </Col>
     
     {/* <Col className="col-lg-4 d-flex align-items-center justify-content-center position-relative"> */}
-    <Col lg={4} className="d-flex align-items-center justify-content-center position-relative">
+    {/* <Col lg={2} className="d-flex align-items-center justify-content-center position-relative">
     <AnimationOnScroll animateIn="animate__bounceIn" delay="100" animateOnce="true">
       <a href="https://www.youtube.com/watch?v=u6BOC7CDUTQ" class="glightbox play-btn"></a>
     </AnimationOnScroll>
-    </Col>
+    </Col> */}
     {/* <div class="col-lg-4 d-flex align-items-center justify-content-center position-relative" data-aos="zoom-in" data-aos-delay="200"> */}
     {/* <a href="https://www.youtube.com/watch?v=u6BOC7CDUTQ" class="glightbox play-btn"></a> */}
     {/* </div> */}
